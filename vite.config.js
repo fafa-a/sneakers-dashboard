9beaf03ba13f9b0cfbc3fd31a8e0f997e4ec3008
fafa-a <fabienpuech1@gmail.com>
2021-07-21 19:18:38 +0200

    chore: initial commit + vite-plugin-components

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+import WindiCSS from "vite-plugin-windicss"
+import ViteComponents from 'vite-plugin-components'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue({ script: { refSugar: true } }), WindiCSS(), ViteComponents()],
+})
